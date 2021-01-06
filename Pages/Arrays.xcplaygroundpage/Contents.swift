02743be8bbcd8a54b701b326fcac92a5d6a7520b
Tsering Lama <tseringglamaa@gmail.com>
2021-01-05 21:23:55 -0500

    finished array exercise

--- Pages/Arrays.xcplaygroundpage/Contents.swift
+++ Pages/Arrays.xcplaygroundpage/Contents.swift
@@ -74,7 +74,7 @@ contacts.removeLast() // crashes if the array is empty
 // removeFirst, popLast(safer bc it removes an optional), removeAll, dropFirst, dropLast
 
 var cities = ["nyc", "ktm"]
-let citiesDropped = cities.dropFirst(5)  // dropLast removes from the back
+let citiesDropped = cities.dropFirst(1)  // dropLast removes from the back
 print(citiesDropped)
 
 //MARK:- ITERATING OVER AN ARRAY
@@ -195,3 +195,161 @@ print(newStr)
 var testString = "apple"
 testString.removeLast()
 print(testString)
+
+
+// MARK: - EXERCISES
+let ques1 = [ "orange", "red", "yellow", "turquoise", "lavender"]
+
+var favColors = [String]()
+
+for (index, color) in ques1.enumerated() {
+    if index % 2 == 0 {
+        favColors.append(color)
+    }
+}
+
+print("\(favColors[0]), \(favColors[1]) and \(favColors[2]) are some of my fav colors")
+
+
+var westernStates = ["California", "Oregon", "Washington", "Idaho", "Illinois", "Kansas"]
+print(westernStates.dropLast(2))
+
+let myString = "This is good practice with Strings!"
+var nonSpaceCount = 0
+for char in myString {
+    if char != " " {
+        nonSpaceCount += 1
+    }
+}
+print(nonSpaceCount)
+
+let myFavoriteQuotes = ["To be or not to be, that is the question.", "The only source of knowledge is experience.", "Mr. Gorbachev, tear down this wall!", "Four score and twenty years ago..."]
+
+for quote in myFavoriteQuotes {
+    var noSpaceCount = 0
+    for str in quote {
+        if str != " " {
+            noSpaceCount += 1
+        }
+    }
+    print(noSpaceCount)
+}
+
+var garden = ["dirt","🌷","dirt","🌷","dirt","dirt","🌷","dirt","🌷","dirt"]
+var basket = [String]()
+
+for (index, thing) in garden.enumerated() {
+    if thing == "🌷" {
+        basket.append(thing)
+        garden[index] = "dirt" // override
+    }
+}
+
+print(basket.count)
+print(garden)
+
+var battingLineup = ["Reyes", "Jeter", "Ramirez", "Pujols","Griffey","Thomas","Jones", "Rodriguez"]
+
+
+
+for (index, batter) in battingLineup.enumerated() {
+    if batter == "Jeter" {
+        battingLineup[index] = "Tejada"
+    }
+    
+    if batter == "Thomas" {
+        battingLineup[index] = "Guerrero"
+    }
+}
+
+if let reyesIndex = battingLineup.firstIndex(of: "Reyes") {
+    battingLineup.remove(at: reyesIndex)
+    battingLineup.insert("Reyes", at: 7)
+}
+
+battingLineup.append("Suzuki")
+print(battingLineup)
+
+var ques7 = [32459,2,4,5,1,4,2,1]
+var target = 3
+var foundTarget = false
+
+for num in ques7 {
+    if num == target {
+        foundTarget = true
+    }
+}
+
+print(foundTarget)
+
+let arrayOfNumbers: [Int] = (1...100).map{ _ in Int.random(in: 0...200)}.map{Int($0)}  // 100 num in between 0 and 200
+
+var largestNum = 0
+var smallestNum = arrayOfNumbers[0]
+
+for num in arrayOfNumbers {
+    if num > largestNum {
+        largestNum = num
+    }
+}
+
+print(largestNum)
+
+for num in arrayOfNumbers {
+    if num < smallestNum {
+        smallestNum = num
+    }
+}
+
+print(smallestNum)
+
+var listOne = [28, 64, 7, 96, 13, 32, 94, 11, 80, 68]
+var listTwo = [18, 94, 48, 6, 42, 68, 79, 76, 13, 7]
+var sharedElements = [Int]()
+
+for num1 in listOne {
+    for num2 in listTwo {
+        if num2 == num1 {
+            sharedElements.append(num2)
+        }
+    }
+}
+
+print(sharedElements)
+
+var dupeFriendlyList = [4,2,6,2,2,6,4,9,2,1]
+var noDupeList: [Int] = []
+
+for num in dupeFriendlyList {
+    if !noDupeList.contains(num) {
+        noDupeList.append(num)
+    }
+}
+
+print(noDupeList)
+
+let arrayOfNumbers2 = [-6, 13, 0, 34, 0, 45, -12, 9, 11, 4]
+
+var secondSmallest = Int.max
+var smallest = Int.max
+
+for num in arrayOfNumbers2 {
+  if num < smallest {
+    secondSmallest = smallest
+    smallest = num
+  } else if num < secondSmallest && num != smallest {
+    secondSmallest = num
+  }
+}
+
+print(smallest, secondSmallest)
+
+var sum16 = 0
+
+for num in 1..<1000 {
+    if num % 3 == 0 || num % 5 == 0 {
+        sum16 += num
+    }
+}
+
+print(sum16)
