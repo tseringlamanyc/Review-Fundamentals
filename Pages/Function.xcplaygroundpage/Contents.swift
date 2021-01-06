7da3ebf1ee7efa31d04419d360a2564d3fbe2d30
Tsering Lama <tseringglamaa@gmail.com>
2021-01-06 11:40:04 -0500

    finished optionals exercises

---
+++ Pages/Function.xcplaygroundpage/Contents.swift
@@ -0,0 +1,113 @@
+import Foundation
+
+// functions: self contained blocks of code that perform a specific task
+
+// defining a function
+
+/*
+ func keyword
+ function name
+ parameter list ()
+ function body {}
+ 
+ function call e.g welcomeGreeting()
+ */
+
+func welcomeGreeting() { // defined the function
+    // body of function
+    print("Welcome to functions in Swift.")
+}
+
+welcomeGreeting() // we called the function does not take arguments
+
+// function parameters and return values
+// input: String
+// output: String
+func add(lastName: String) -> String {
+    return "Your last name is \(lastName)"
+}
+
+// call the add function
+let result = add(lastName: "Appleseed")
+print(result)
+
+// functions with multiple parameters
+func add(number1: Int, number2: Int) -> Int {
+    return number1 + number2
+}
+let result2 = add(number1: 5, number2: 10)
+print(result2)
+
+// functions without return values
+func popularProgrammingLanguage(language: String) {
+    print("Popular language is \(language)")
+}
+
+popularProgrammingLanguage(language: "Swift")
+
+
+// functions with multiple return values
+func getCoordinate() -> (latitude: Double, longitude: Double) {
+    return (40.3, 73.5)
+}
+
+let coordinate = getCoordinate()
+print(coordinate.longitude)
+
+// function with implicit return
+func multiply(num1: Int, num2: Int) -> Int {
+    //num1 * num2 - not support in this version on repl
+    return num1 * num2
+}
+
+let result3 = multiply(num1: 10, num2: 8)
+print(result3)
+
+// function argument labels and parameter names
+func getFirstName(for person: String) -> String {
+    let name = person.split(separator: " ")
+    return String(name[0])
+}
+
+let firstName = getFirstName(for: "Alex Paul") // => "Alex" "Paul"
+print(firstName)
+
+// omitting argument labels
+func square(_ num: Int) -> Int {
+    //num * num // implicit return - not supported in this version of repl
+    return num * num // implicit return
+}
+
+let result4 = square(9)
+
+// default parameter values
+func getCoffee(sugar: Int = 1) {
+    print("Here's your coffee, \(sugar) sugar was added.")
+}
+
+getCoffee() // will use a default argument
+getCoffee(sugar: 3)
+
+// variadic parameters
+func sum(_ values: Int...) {
+    let result = values.reduce(0, +)
+    print("The sum of values is \(result)")
+}
+
+sum(5, 10, 2, 10)
+
+// in-out parameters
+func change(name: inout String) {
+    // by default parameters in a function are constants
+    // cannot be changed
+    name = "John Appleseed"
+    // name here is locally scoped to the change() function
+}
+
+var personName = "Tim Cook"
+
+change(name: &personName)
+
+print(personName)
+
+
--- Pages/Optionals.xcplaygroundpage/Contents.swift
+++ Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -72,6 +72,8 @@ func printNode(node: Node?) {
     }
 }
 
+printNode(node: node1)
+
 //MARK:- OPTIONAL CHAINING
 
 var userName: String?
@@ -86,3 +88,73 @@ if let username = userName,
     print("values are missing")
 }
 
+//MARK:- EXERCISE
+var userNameOne: String? = "Test User"
+
+if let userNameOne = userNameOne {
+    print("The username is the \(userNameOne)")
+}
+
+var userNameTwo: String? = nil
+print("The username is \(userNameTwo ?? "undefined")")
+
+
+var rectOneWidth: Double? = 5
+var rectOneHeight: Double? = 10
+
+if let rectOneWidth = rectOneWidth, let rectOneHeight = rectOneHeight {
+    print("The area is \(rectOneWidth * rectOneHeight)")
+}
+
+var rectTwoWidth: Double? = nil
+var rectTwoHeight: Double? = nil
+
+if let rectTwoWidth = rectTwoWidth, let rectTwoHeight = rectTwoHeight {
+    print("The area is \(rectTwoWidth * rectTwoHeight)")
+} else {
+    print("Area couldnt be calculated")
+}
+
+var userOneName: String? = "Anne"
+var userOneAge: Int? = 15
+var userOneHeight: Double? = 70
+
+if let userOneName = userOneName, let userOneAge = userOneAge, let userOneHeight = (userOneHeight) {
+    print("Hi \(userOneName). You are \(userOneAge) and \(userOneHeight / 12) tall")
+}
+
+var favoriteNumber = Bool.random() ? Int.random(in: 0...10) : nil
+
+if let favoriteNumber = favoriteNumber {
+    print("Fav num is \(favoriteNumber)")
+} else {
+    print("no number")
+}
+
+var numOne = Bool.random() ? Int.random(in: 0...10) : nil
+var numTwo = Bool.random() ? Int.random(in: 0...10) : nil
+var numThree = Bool.random() ? Int.random(in: 0...10) : nil
+
+if let numOne = numOne, let numTwo = numTwo, let numThree = numThree {
+    print("The total sum is \(numOne + numTwo + numThree)")
+} else {
+    print("Couldnt calculate sum")
+}
+
+var numbers6 = [Int?]()
+
+for _ in 0..<10 {
+    numbers6.append(Bool.random() ? Int.random(in: 0...100) : nil)
+}
+
+var sum = 0
+
+for num in numbers6 {
+    if let validNum = num {
+        sum += validNum
+    }
+}
+
+print("The sum is \(sum)")
+
+print(numbers6.compactMap { $0 }.reduce(0, +))
